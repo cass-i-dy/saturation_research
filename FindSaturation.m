94ece1cf3abc95d8f8748ae962d837d9af3c4fa1
cass-i-dy <120527008+cass-i-dy@users.noreply.github.com>
2024-05-21 19:10:13 -0600

    adding starting saturation class

---
+++ FindSaturation.m
@@ -0,0 +1,23 @@
+classdef FindSaturation
+    %PURPOSE To find if a water sample is over or undersaturated given
+    %input data
+    %   Detailed explanation goes here
+
+    properties
+        Property1
+    end
+
+    methods
+        function obj = untitled(inputArg1,inputArg2)
+            %UNTITLED Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.Property1 = inputArg1 + inputArg2;
+        end
+
+        function outputArg = method1(obj,inputArg)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            outputArg = obj.Property1 + inputArg;
+        end
+    end
+end
